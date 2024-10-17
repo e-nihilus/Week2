03d313655e873341d004242d0d8b7a378be69d9c
e-nihilus <epardorayo@gmail.com>
2024-10-17 12:17:17 +0200

    Primera subida de los ejercicios de repaso final del reto de bucles for

---
+++ retos1/buclesFor.ts
@@ -0,0 +1,61 @@
+// 5. Realizar una función que te devuelva la suma del numero de caracteres de las palabras
+// almacenadas en un array.
+// La cabecera de la función tendrá el siguiente aspecto: function add(myWords)
+
+//Declaracion funciones
+function evenNumbers(num:number){
+    for (let i=0;  i<=num; i++){
+        if (i % 2 !=0){
+            console.log(i);
+        }
+    }
+}
+
+function  myRevert(myArr:string[]):string[]{
+    let revertA: string[] = [];
+    for (let i = myArr.length -1; i >= 0; i--){
+        revertA.push(myArr[i]);
+    }
+    return revertA;
+}
+
+function isRainbow(colors: string[]){
+    let rainbow: string[] = ["rojo", "naranja", "amarillo", "verde","azul", "andigo", "violeta"];
+
+    for (let i = 0; i < colors.length; i++){
+        if (rainbow.includes(colors[i])){
+            console.log(colors[i] + " está en el arcoiris");
+        }
+        else{
+            console.log(colors[i] + " no está en el arcoiris");
+        }
+    }
+  
+}
+
+function add(myWords: string[]):number{
+    let sumPal: number = 0;
+    for ( let i=0; i<myWords.length; i++){
+        sumPal += myWords[i].length;
+    }
+    return sumPal;
+}
+
+console.log("----------Reto1----------");
+
+evenNumbers(10)
+
+console.log("----------Reto2----------");
+
+let myArr: string[] = ["Hola", "que", "tal", "estas", "?"]
+
+console.log(myRevert(myArr));
+
+console.log("----------Reto3----------");
+
+let colors: string[] = ["rojo", "verde", "negro", "blanco", "azul"]
+isRainbow(colors);
+
+console.log("----------Reto4----------");
+
+console.log(add(myArr))
---
+++ retos1/condicionales.ts
@@ -0,0 +1,146 @@
+//Declaracion de funciones
+function zodiac(day: number, month: number): string {
+    let sign: string = "";
+    if (month === 1) {
+        if (day>=20){
+            sign = "Acuario"
+        }
+        else{
+            sign ="Capricornio"
+        }
+    }
+    else if (month === 2) {
+        if (day>=20){
+            sign = "Piscis"
+        }
+        else{
+            sign ="Acuario"
+        }
+    }
+    else if (month === 3) {
+        if (day>=20){
+            sign = "Aries"
+        }
+        else{
+            sign ="Piscis"
+        }
+    }
+    else if (month === 4) {
+        if (day>=20){
+            sign = "Tauro"
+        }
+        else{
+            sign ="Aries"
+        }
+    }
+    else if (month === 5) {
+        if (day>=20){
+            sign = "Geminis"
+        }
+        else{
+            sign ="Tauro"
+        }
+    }
+    else if (month === 6) {
+        if (day>=20){
+            sign = "Cancer"
+        }
+        else{
+            sign ="Geminis"
+        }
+    }
+    else if (month === 7) {
+        if (day>=20){
+            sign = "Leo"
+        }
+        else{
+            sign ="Cancer"
+        }
+    }
+    else if (month === 8) {
+        if (day>=20){
+            sign = "Virgo"
+        }
+        else{
+            sign ="Leo"
+        }
+    }
+    else if (month === 9) {
+        if (day>=20){
+            sign = "Libra"
+        }
+        else{
+            sign ="Virgo"
+        }
+    }
+    else if (month === 10) {
+        if (day>=20){
+            sign = "Escorpio"
+        }
+        else{
+            sign ="Libra"
+        }
+    }
+    else if (month === 11) {
+        if (day>=20){
+            sign = "Sagitario"
+        }
+        else{
+            sign ="Escorpio"
+        }
+    }
+    else if (month === 12) {
+        if (day>=20){
+            sign = "Capricornio"
+        }
+        else{
+            sign ="Sagitario"
+        }
+    }
+    else {
+        sign = "Datos no validos";
+    }
+    return sign;
+}
+
+function  continent(country: string){
+    let continentes = {
+        "Africa": ["Egipto", "Sudáfrica", "Tanzania","Marruecos", "Nigeria"],
+        "Asia": ["China", "India", "Japón", "Corea del Sur", "Indonesia"],
+        "Europa":  ["España", "Francia", "Alemania", "Italia", "Reino Unido"],
+        "Norteamérica": ["Estados Unidos", "Canadá", "México", "Cuba", "Costa Rica"],
+        "Suramerica":  ["Brasil", "Argentina", "Chile", "Perú", "Colombia"],
+        "Oceanía": ["Australia", "Nueva Zelanda", "Fiyi"]
+    }
+    for (let conti in continentes){
+        if (continentes[conti].includes(country)){
+            console.log(conti);
+        }
+    }
+}
+
+function isEven(number:number){
+    if  (number % 2 === 0){
+        console.log("El numero es par");
+    }
+    else{
+        console.log("El numero es impar");
+    }
+}
+
+console.log("----------Reto1----------");
+
+console.log(zodiac(8, 1));
+
+console.log("----------Reto2----------");
+
+continent("España");
+
+console.log("----------Reto3----------");
+
+isEven(333);
+
+
+
+
+
